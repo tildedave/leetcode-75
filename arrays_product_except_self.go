7e447f19bf7a82a920e6b9bfe25aace05ccca30c
Dave King <tildedave@gmail.com>
2023-07-22 14:08:33 -0400

    Arrays: product except self

---
+++ arrays_product_except_self.go
@@ -0,0 +1,50 @@
+package leetcode_75
+
+/*
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+*/
+
+// Obviously using division makes it trivial
+// product[0] = nums[1] * nums[2] * nums[3]
+// product[1] = nums[0] * nums[2] * nums[3]
+// product[2] = nums[0] * nums[1] * nums[3]
+// product[3] = nums[0] * nums[1] * nums[2]
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+// OK so the idea is getting two arrays, product prefix / product suffix
+
+// Prefix: [1, 1, 2, 6]
+// Suffix: [24, 12, 4, 1]
+// Then the result is prefix * suffix.
+
+func productExceptSelf(nums []int) []int {
+	var productPrefix []int = make([]int, len(nums))
+	var productSuffix []int = make([]int, len(nums))
+
+	var prefixSoFar int = 1
+	var suffixSoFar int = 1
+	var n = len(nums) - 1
+	for i, _ := range nums {
+		if i == 0 {
+			productPrefix[i] = 1
+			productSuffix[n - i] = 1
+			continue
+		}
+		prefixSoFar *= nums[i - 1]
+		suffixSoFar *= nums[n - i + 1]
+		productPrefix[i] = prefixSoFar
+		productSuffix[n - i] = suffixSoFar
+	}
+
+	var result []int = make([]int, len(nums))
+	for i, _ := range nums {
+		result[i] = productPrefix[i] * productSuffix[i]
+	}
+	return result
+}
